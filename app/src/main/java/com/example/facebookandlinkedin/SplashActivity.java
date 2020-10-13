edb6265c4b6ec4e736a8ad5e34b7d35c323b669b
teja vutla <54974238+tejavutla@users.noreply.github.com>
2020-10-13 19:02:52 +0530

    files upload

---
+++ app/src/main/java/com/example/facebookandlinkedin/SplashActivity.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2017. http://hiteshsahu.com- All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * If you use or distribute this project then you MUST ADD A COPY OF LICENCE
+ * along with the project.
+ *  Written by Hitesh Sahu <hiteshkrsahu@Gmail.com>, 2017.
+ */
+
+package com.example.facebookandlinkedin;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.Handler;
+
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.fragment.app.FragmentActivity;
+
+
+public class SplashActivity extends FragmentActivity {
+
+    private Animation animation;
+    private ImageView logo,logo1;
+    private TextView appTitle;
+    private TextView appSlogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        logo = (ImageView) findViewById(R.id.logo_img);
+        logo1 = (ImageView) findViewById(R.id.logo1_img);
+        appSlogan = (TextView) findViewById(R.id.pro_txt);
+
+
+
+        if (savedInstanceState == null) {
+            flyIn();
+        }
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                endSplash();
+            }
+        }, 3000);
+    }
+
+    private void flyIn() {
+        animation = AnimationUtils.loadAnimation(this, R.anim.logo_animation);
+        logo.startAnimation(animation);
+        logo1.startAnimation(animation);
+
+        animation = AnimationUtils.loadAnimation(this,
+                R.anim.app_name_animation);
+
+        animation = AnimationUtils.loadAnimation(this, R.anim.pro_animation);
+        appSlogan.startAnimation(animation);
+    }
+
+    private void endSplash() {
+        animation = AnimationUtils.loadAnimation(this,
+                R.anim.logo_animation_back);
+        logo.startAnimation(animation);
+        logo1.startAnimation(animation);
+
+        animation = AnimationUtils.loadAnimation(this,
+                R.anim.app_name_animation_back);
+
+        animation = AnimationUtils.loadAnimation(this,
+                R.anim.pro_animation_back);
+        appSlogan.startAnimation(animation);
+
+        animation.setAnimationListener(new AnimationListener() {
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+
+                Intent intent = new Intent(getApplicationContext(),
+                        MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation arg0) {
+            }
+
+            @Override
+            public void onAnimationStart(Animation arg0) {
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Do nothing
+    }
+
+}
